354600bc7acf5044e2ef1e4309077ed39d90e2c9
双下巴真的不可爱 <15230512298@163.com>
2022-11-14 09:54:10 +0800

    更新

--- src/main/java/idata/platform/privacy/ServicePrivacyApplication.java
+++ src/main/java/idata/platform/privacy/ServicePrivacyApplication.java
@@ -14,6 +14,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ServicePrivacyApplication {
     public static void main(String[] args){
         SpringApplication.run(ServicePrivacyApplication.class, args);
-
     }
 }
